ebf9bf6b9eb65a0b772fc8e19af08492a90c74fc
PatelPriyanka <patelpriyanka332@gmail.com>
2018-11-04 13:45:43 -0500

    added sql file

---
+++ newuser.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE hibernate;
+USE hibernate;
+CREATE TABLE newuser(id int(10) PRIMARY KEY, fname varchar(25),lname varchar(22),emailid varchar(25),phone_no varchar(10), nationality varchar(25) );
